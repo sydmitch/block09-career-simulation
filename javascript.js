6879405191c5a16fd10a0843e8c02f2942cf446f
Sydney Mitchell <Sydney@Mac.attlocal.net>
2024-12-03 11:06:47 -0600

    added html form and js files

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
-// insert Hello, World! message in console //
+// insert Hello, World! message in console 
 const greeting = "Hello, world!"
 console.log(greeting)
+
+// code from rubric to show success window in success.html 
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
